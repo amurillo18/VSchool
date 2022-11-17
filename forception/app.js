2ced4b83a6f2d1394d9f40d9f88243a66dc39769
amurillo18 <80285729+amurillo18@users.noreply.github.com>
2022-11-17 13:44:27 -0800

    Add files via upload

---
+++ forception/app.js
@@ -0,0 +1,17 @@
+let peopleAlpha = [];
+
+function forception(people, alphabet){
+    var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for(let i = 0; i < people.length;i++) 
+    {
+         peopleAlpha.push(people[i] + ":");
+         for(let j = 0; j < alphabet.length; j++) {
+            peopleAlpha.push(alphabet[j])
+         }
+        
+    } return peopleAlpha;
+}
+console.log(forception());
+// Output:
+// ["Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
